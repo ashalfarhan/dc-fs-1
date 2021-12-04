15bfce76331e1478b8e03460f833faa6d4afc738
ashalfarhan <ashalfarhan8@gmail.com>
2021-12-04 19:36:30 +0700

    error handling web

--- workspaces/web/src/utils/upload.ts
+++ workspaces/web/src/utils/upload.ts
@@ -2,5 +2,9 @@ export function upload(form: FormData) {
   return fetch('/api/images', {
     body: form,
     method: 'POST',
-  }).then((res) => (res.ok ? res.json() : Promise.reject()))
+  }).then(async (res) => {
+    const response = await res.json()
+    if (res.ok) return response
+    throw new Error(response.message)
+  })
 }
